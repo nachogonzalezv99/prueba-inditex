a4fd63cd4249d80307cb7d9b85a28c65bb095520
Nacho Gonzalez <nachogonzalez.v.99@gmail.com>
2023-01-25 12:09:33 +0100

    Refactored ProductList with custom hook

--- src/hooks/useProductList.js
+++ src/hooks/useProductList.js
@@ -1,5 +1,30 @@
+import { useEffect, useMemo, useState } from "react";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
+import { useSelector } from "react-redux/es/exports";
+import {getProductsRedux} from "../features/productsSlice"
 
 export const useProductList = () => {
+  const dispatch = useDispatch();
+  const { products } = useSelector((store) => store.poducts);
+  const [search, setSearch] = useState([]);
+  
+  const filteredProducts = useMemo(() => {
+    return products.filter((product) => {
+      if (
+        product.marca.match(new RegExp(search, "i")) ||
+        product.modelo.match(new RegExp(search, "i"))
+      ) {
+        return product;
+      }
+    });
+  }, [products, search]);
 
-  return {};
+  useEffect(() => {
+    dispatch(getProductsRedux());
+  }, []);
+
+  const onSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+  return { filteredProducts, onSearchChange, search };
 };
--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,33 +1,11 @@
-import { useEffect, useMemo, useState } from 'react'
 import { ProductCard } from '../../components/ProductCard/ProductCard'
 import { Search } from "../../components/Search/Search"
-import { getProductsRedux } from '../../features/productsSlice'
+import { useProductList } from '../../hooks/useProductList'
 import styles from './ProductList.module.scss'
-import { useDispatch } from 'react-redux/es/hooks/useDispatch'
-import { useSelector } from 'react-redux/es/exports'
 
 const ProductList = () => {
-
-  const dispatch = useDispatch()
-  const { products } = useSelector((store) => store.poducts)
-  const [search, setSearch] = useState([])
-  const filteredProducts = useMemo(() => {
-    return products.filter(product => {
-      if ((product.marca).match(new RegExp(search, 'i')) ||
-        (product.modelo).match(new RegExp(search, 'i'))) {
-        return product
-      }
-    })
-  }, [products, search])
-
-  useEffect(() => {
-    dispatch(getProductsRedux())
-  }, [])
-
-  const onSearchChange = (e) => {
-    setSearch(e.target.value)
-  }
-
+  
+  const { filteredProducts, onSearchChange, search } = useProductList()
 
   return (
     <div className={`${styles.productList} container section`}>
